d2b13b320649fa64941546801c4453c21d202785
Hari Loan <nguyenthimyloan061196@gmail.com>
2017-04-12 16:46:55 +0700

    get/post

---
+++ demo/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace demo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = "1" },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = "6" },
+            new Product { Id = 3, Name = "Milk", Category = "Hardware", Price = "16" }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        [HttpPost]
+        public string PostProduct(Product product)
+        {
+            return String.Format("{0} {1} {2} {3}", product.Id, product.Name, product.Category, product.Price);
+        }
+
+    }
+}
